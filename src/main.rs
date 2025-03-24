8892f10d0e0b5ad6423f69ac71c7ecb0576af736
Raymond Cheung <korekontakt@gmail.com>
2025-03-24 17:49:09 +0000

    Add Tortue::new()

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use macroquad::prelude::*;
 
+mod tortue;  // Tell Rust to use tortue.rs
+use tortue::Tortue;
+
 #[macroquad::main("Tortue")]
 async fn main() {
+    let mut tortue = Tortue::new();
     loop {
         clear_background(WHITE);
+        draw_circle(tortue.x, tortue.y, 5.0, RED);  // Show tortue as a red dot
         next_frame().await;
     }
 }
--- src/tortue.rs
+++ src/tortue.rs
@@ -3,3 +3,13 @@ pub struct Tortue {
     pub y: f32,
     pub angle: f32,
 }
+
+impl Tortue {
+    pub fn new() -> Self {
+        Tortue {
+            x: 400.0, // Centre of 800x600 window
+            y: 300.0,
+            angle: 0.0, // Facing right (0 degrees)
+        }
+    }
+}
