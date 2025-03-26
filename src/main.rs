17addc7f7c2f151bac901afd2e28df31f2800757
Raymond Cheung <korekontakt@gmail.com>
2025-03-26 15:33:16 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,11 @@ use tortue::Tortue;
 #[macroquad::main("Tortue")]
 async fn main() {
     let mut tortue = Tortue::new();
-    tortue.forward(100.0); // Right
-    tortue.right(90.0); // Turn down
-    tortue.forward(100.0); // Down
-    tortue.right(90.0); // Turn left
-    tortue.forward(100.0); // Left
-    tortue.right(90.0); // Turn up
-    tortue.forward(100.0); // Up
-    loop {
-        tortue.draw();
-        next_frame().await;
+
+    for _ in 0..360 {
+        tortue.forward(1.0);
+        tortue.right(1.0);
     }
+
+    tortue.draw().await;
 }
